8222cd9a8f11202e0357ec74ac8c9022b8237145
Kalkidan Melaku Hailemariam <kalimelakuu>
2022-09-16 01:08:52 -0500

    Add From Product to Cart Functionalities

--- src/Component/card-dropdown/card-dropdown.component.jsx
+++ src/Component/card-dropdown/card-dropdown.component.jsx
@@ -1,11 +1,22 @@
 import Button from '../button/button.component'
+import CartItem from '../cart-item/cart-item.component'
 import './card-dropdown.style.scss'
+import { useContext } from 'react'
+import { CartContext } from '../../contexts/cart.context'
+
 
 const CartDropDown = () => {
 
+    const {cartItems} = useContext(CartContext);
     return (
         <div className='cart-dropdown-container '>
            <div className='cart-items '/>
+           { cartItems.length ?
+            (cartItems.map(item => 
+                 (<CartItem key={item.id} cartItem={item}/>))) :
+                 (<span className='empty-message'>Your cart is empty</span>)
+           }
+           
            <Button>GO TO CHECKOUT</Button>
         </div>
     )
